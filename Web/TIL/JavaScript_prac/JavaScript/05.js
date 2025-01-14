44f847cf3c6becbdfa9611dc696100458129c84d
Yoonseok Choi <yoonseok0216@gmail.com>
2025-01-14 09:32:31 +0900

    01/14(1)

---
+++ Web/TIL/JavaScript_prac/JavaScript/05.js
@@ -0,0 +1,9 @@
+let fruits = ['apple', 'orange', 'berry'];
+console.log(fruits); // ['apple', 'orange', 'berry']
+console.log(fruits[0]);
+
+let numbers = [1, 2, '3'];
+console.log(numbers); // [ 1, 2, '3' ]
+console.log(numbers[0]);
+numbers.push(4);
+console.log(numbers)
