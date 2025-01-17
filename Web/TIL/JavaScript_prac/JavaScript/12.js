19e41e28d358bf6b084e0e24df66315a61a82d7e
Yoonseok Choi <yoonseok0216@gmail.com>
2025-01-17 16:03:08 +0900

    01/17

--- Web/TIL/JavaScript_prac/JavaScript/12.js
+++ Web/TIL/JavaScript_prac/JavaScript/12.js
@@ -2,19 +2,36 @@
 //             .then(response => response.json())
 //             .then(res => console.log(res))
 
-console.log('A');
-setTimeout(() => {
-    console.log('B'); // 함수 실행 후 1초 후에 실행된다.
-}, 5000);
-console.log('C');
+// console.log('A');
+// setTimeout(() => {
+//     console.log('B'); // 함수 실행 후 1초 후에 실행된다.
+// }, 5000);
+// console.log('C');
 
 // setInterval
-let number = 0;
+// let number = 0;
+// setInterval(() => {
+//     number += 1;
+//     if (number<=10){
+//       const newDiv = document.querySelector("#id1")
+//       newDiv.textContent = number
+//       console.log(number);
+//     }
+// }, 1000);
 
-setInterval(() => {
-  // 1초마다 아래 코드가 실행된다.
-    console.log(number);
-    number = number + 1;
-}, 1000);
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
-console.log('Hello World');
+const data = fetch(url)
+    .then((response) => {
+        // 네트워크 요청 결과 데이터를 JSON 구조로 변환 후 반환
+        return response.json();
+    })
+    .then((json) => {
+        // JSON 구조로 변환된 데이터 처리
+        console.log(json);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+// { userId: 1, id: 1, title: 'delectus aut autem', completed: false }
